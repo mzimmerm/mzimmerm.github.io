9df8f926d052a472c877c6ca3752f19749d7bda3
Milan Zimmermann <milan.zimmermann@gmail.com>
2021-06-14 14:56:38 -0400

    Updates in newspeak blogs, also removed prematurely pushed probability blog.

--- my-serve-http-jekyll-my-blog.sh
+++ my-serve-http-jekyll-my-blog.sh
@@ -1,4 +1,15 @@
 #!/bin/sh -e
+
+# Script used from my systemd service my-serve-http-jekyll-my-blog.service
+
+# This watches for changes in $BLOG.
+# When a blog is added or modified using org2jekyll-publish, two things happen:
+#   1. Org-export runs, which exports the blog $BLOG/org/a-test-blog.org to html
+#      and places the html to the $BLOG/_posts/2021-04-20-a-test-blog.html
+#   2. The Jekyll auto-watch will then copy all "special" dirs from _posts to _site, in detail:
+#      Copies $BLOG/_posts/2021-04-20-a-test-blog.html to $BLOG/_site/2021/04/20/a-test-blog.html
+#   3. Blog index is built from the _posts directory, not as we would expect from the _site directory.
+
 # cd $BLOG
 cd /home/mzimmermann/dev/my-projects-source/public-on-github/mzimmerm.github.io
 echo path is $(pwd)
